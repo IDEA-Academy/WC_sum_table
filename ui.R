a3c81281ed8f983d21fc8c58b28a4e88cc014dcb
emmamclarnon <35924275+emmamclarnon@users.noreply.github.com>
2018-01-31 07:30:06 +0000

    Add files via upload
    Working scripts, with layout different to previous version.  Word cloud not in a good position, nut removed the tab for the URL table.

--- ui.R
+++ ui.R
@@ -3,58 +3,71 @@
 fluidPage(
   
   titlePanel("Deskpro Issues"),
-  # Create a new Row in the UI for selectInputs
-  fluidRow(
+  ## word cloud   
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("selection", "Choose a service:",
+                       c("All",
+                         unique(as.character(Deskpro$Service)))),
+           actionButton("update", "Change"),
+           hr(),
+           sliderInput("freq",
+                       "Minimum Frequency:",
+                       min = 1,  max = 50, value = 15),
+           sliderInput("max",
+                       "Maximum Number of Words:",
+                       min = 1,  max = 300,  value = 100)
+      ),
+
+      
+  #)
+           
+    #conditionalPanel(
+    # 'input.dataset === "Deskpro"',
+    # helpText("Click the column header to sort a column."),
+    
+
+    
+   # for URL table
+#  fluidRow(
+#    column(3,
+#           selectInput("Service",
+#                       "Service:",
+##                       c("All",
+  #                       unique(as.character(URL_count$Service)))))
+#  ),
+  
+   # mainPanel(
+  #    column(1,
+    #  plotOutput("plot", "500px","500px")
+ # ),
+
+fluidRow(
     column(2,
            selectInput("Service",
                        "Service:",
                        c("All",
-                         unique(as.character(URL_count$Service)))))
-  ),
-  #column(1,
-  #       selectInput("Ticket",
-  #                   "Type of request:",
-  #                   c("All",
-  #                     unique(as.character(URL_count$Ticket))))
-  # )
-  # ),
-  # Create a new row for the table.
-  fluidRow(
-    DT::dataTableOutput("table")
-  ),
+                         unique(as.character(URL_count$Service)))),       
+           DT::dataTableOutput("table")
+           ),
+    
+    fluidRow(
+      column(1, 
+             plotOutput("plot", "400px","600px")
+      )
+      
   
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("selection", "Choose a service:",
-                  c("All",
-                    unique(as.character(Deskpro$Service)))),
-      actionButton("update", "Change"),
-      hr(),
-      sliderInput("freq",
-                  "Minimum Frequency:",
-                  min = 1,  max = 50, value = 15),
-      sliderInput("max",
-                  "Maximum Number of Words:",
-                  min = 1,  max = 300,  value = 100)
-    ),
-  #conditionalPanel(
-  # 'input.dataset === "Deskpro"',
-  # helpText("Click the column header to sort a column."),
-  
-  mainPanel(
-    plotOutput("plot", "800px", "600px"),
     # mainPanel(
     #   plotOutput("words_Plot"),
-    mainPanel(
-      tabsetPanel(
-        id = 'URL_count',
-        tabPanel("URL_count", DT::dataTableOutput("mytable1"))
+   # mainPanel(
+    #  tabsetPanel(
+     #   id = 'URL_count',
+      #  tabPanel("URL_count", DT::dataTableOutput("mytable1"))
         
         
-        # )
          )
+        )
       )
-    )  
-  )
-)
+   )  
+  #)
+#)
